dffb34430c8890e0240657bcf7e7e8db0e863181
CagiestAce <guellicaio@gmail.com>
2024-09-07 18:14:23 -0300

    Primeiro commit

---
+++ dist/js/FormatoData.js
@@ -0,0 +1,6 @@
+var FormatoData;
+(function (FormatoData) {
+    FormatoData["PADRAO"] = "DD/MM/AAAA";
+    FormatoData["DIA_SEMANA_DIA_MES_ANOS"] = "DIA_SEMANA, DD/MM/AAAA";
+    FormatoData["DIA_MES"] = "DD/MM";
+})(FormatoData || (FormatoData = {}));
---
+++ src/FormatoData.ts
@@ -0,0 +1,5 @@
+enum FormatoData {
+    PADRAO = "DD/MM/AAAA",
+    DIA_SEMANA_DIA_MES_ANOS = "DIA_SEMANA, DD/MM/AAAA",
+    DIA_MES = "DD/MM"
+}
